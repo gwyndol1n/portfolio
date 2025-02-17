94109a36e6b89c1686a2dcfa5471157cfed4cd88
Gwyn Moon <gwyndolinmaria@gmail.com>
2025-02-17 12:21:28 -0600

    Updates

---
+++ precache-manifest.a397ba887897d539f35cb94132f34e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3549f80a2d684cd011fa687c440ace",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17ad7fa93836c39e183d",
+    "url": "/portfolio/static/css/main.b6abab66.chunk.css"
+  },
+  {
+    "revision": "ecc8d361e73e4d6adc6b",
+    "url": "/portfolio/static/js/2.ea17ebe1.chunk.js"
+  },
+  {
+    "revision": "20ec4c7a76524148d680",
+    "url": "/portfolio/static/js/3.132715ea.chunk.js"
+  },
+  {
+    "revision": "17ad7fa93836c39e183d",
+    "url": "/portfolio/static/js/main.0be755b4.chunk.js"
+  },
+  {
+    "revision": "8840a3ed8f8f06e05ab4",
+    "url": "/portfolio/static/js/runtime~main.c45ed965.js"
+  }
+]);
